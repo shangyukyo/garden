d0bf16866cd2d8136baa0c9d423772c00c1f7a23
shangyukyo <sdzc1014@163.com>
2015-07-30 15:31:47 +0800

    init

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,6 +9,6 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 
+Rails.application.config.asset_host = (Rails.env.production? ? "http://101.200.197.16" : "http://localhost:3000")
 
-Rails.application.config.asset_host = "http://localhost:3000"
 Rails.application.config.assets.precompile += %w( login.css goods.js good_spec.js)
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,3 +1,3 @@
 ::CarrierWave.configure do |config|  
-  config.asset_host = 'http://localhost:3000'
+  config.asset_host = Rails.env.production? ? "http://101.200.197.16" : "http://localhost:3000"
 end
