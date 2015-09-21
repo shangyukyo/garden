f763e80a0402d6da69028fb034446ce2ebdc07d1
shangyukyo <sdzc1014@163.com>
2015-09-21 10:16:42 +0800

    fixbug

--- app/controllers/api/posters_controller.rb
+++ app/controllers/api/posters_controller.rb
@@ -2,6 +2,11 @@ class Api::PostersController < Api::ApplicationController
 
   def index    
     @posters = Poster.all.order('id desc')
+
+    @posters = @posters.map{|p|
+      next if not p.good.present?
+      p
+    }
   end
 
 end
--- app/views/api/posters/_poster.json.jbuilder
+++ app/views/api/posters/_poster.json.jbuilder
@@ -1,5 +1,5 @@
 json.(poster, :id, :photo_url, :good_id, :created_at, :updated_at)
 
-json.good do 
+json.good do  
   json.partial! '/api/goods/good', good: poster.good
 end
