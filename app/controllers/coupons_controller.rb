ae62b5ae4449b62c1ee8423c1b3ea69d89f706a9
shangyukyo <sdzc1014@163.com>
2015-10-29 09:58:13 +0800

    fixbug

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -56,6 +56,44 @@ class CouponsController < ApplicationController
     
   end
 
+  def edit
+    @coupon = Coupon.find(params[:id])
+  end
+
+  def update
+    
+    params.permit!      
+    @coupon = Coupon.find(params[:id])
+    @coupon.name = params[:name]
+    @coupon.price = params[:price].to_f
+    @coupon.minimum = params[:minimum].to_f
+    @coupon.start_at = params[:start_at]
+    @coupon.expired_at = params[:expired_at]
+
+    if not params[:start_at].present?
+      redirect_to :back, alert: '有效期 开始时间不能为空' and return
+    end
+
+    if not params[:expired_at].present?
+      redirect_to :back, alert: '有效期 结束时间不能为空' and return
+    end    
+
+    if not params[:price].to_f > 0
+      redirect_to :back, alert: '价值必须大于0' and return
+    end
+
+
+    if not params[:name].present?
+      redirect_to :back, alert: '名称不能为空' and return
+    end
+
+    if @coupon.save!
+      redirect_to coupons_path(coupon_type: @coupon.coupon_type)
+    else
+      redirect_to :back, alert: @coupon.errors.full_messages
+    end    
+  end
+
   def destroy
     redirect_to :back if Coupon.find(params[:id]).destroy
   end
